93c519b754e828d24eef992ddbeace6de3bda35a
igorsudijovski@gmail.com <igorsudijovski@gmail.com@04d6a1da-28f5-baf6-761c-ae809e26149c>
2013-01-08 12:50:21 +0000

    Zaedno so nov semestar

---  igra-finki --username IgorSudijovski@gmail.com/Seminarska/js/test.js
+++  igra-finki --username IgorSudijovski@gmail.com/Seminarska/js/test.js
@@ -1,19 +1,22 @@
 paper.install(window);
 window.onload = function(){
-	
 	paper.setup('myCanvas');
+	var accordion = 0;
+	var canvas = document.getElementById('myCanvas');
 	var draw = false;
-	
-	var background = new Path.Rectangle(new Rectangle(new Point(0,0),new Size(1000,1000)));
+	var background = new Path.Rectangle(new Rectangle(new Point(0,0),new Size(canvas.width,canvas.height)));
 	background.fillColor = "#EEEEEE";
 	var ply;
-	var basex = 0;
-	
+	var basex = 0;	
 	var ball = new Ball();
 	var tool = new Tool();
 	var base = new Base();
-	base.init(500);
-	jQuery("#accordion").accordion();
+	jQuery("#reinit").click(function(){
+		ply.reinit();
+		draw = true;
+	});
+	base.init(canvas.height);
+	jQuery("#accordion").accordion({event:"mouseclick"});
 	jQuery("#dialog").dialog({
 		autoOpen: true,
 		height: 500,
@@ -23,12 +26,13 @@ window.onload = function(){
 		buttons:[{
 			text: "Потврди",
 			click: function(e){
-				var predmeti1z = [];
+				if(accordion == 3){
+					var predmeti1z = [];
 				var zad = jQuery("#prva ul li");
 				for(var i = 0; i < zad.length; i++){
-					var ID = zad[i].firstChild.attributes[0].nodeValue;
+					var ID = zad[i].firstChild.getAttribute("title");
 					var name = zad[i].firstChild.innerHTML;
-					var pre = new Predmet(ID.split(" ")[0], name, ID.split(" ")[1]);
+					var pre = new Predmet(ID.split(" ")[0], name, parseInt(ID.split(" ")[1]));
 					pre.otvoren = true;
 					predmeti1z.push(pre);
 				}
@@ -36,9 +40,9 @@ window.onload = function(){
 				var predmeti1i = [];
 				for(var i = 0; i < izb.length; i++){
 					if(izb[i].checked){
-						var ID = izb[i].attributes[1].nodeValue;
+						var ID = izb[i].getAttribute("title");
 						var name = izb[i].nextSibling.nodeValue;
-						var pre = new Predmet(ID.split(" ")[0], name, ID.split(" ")[1]);
+						var pre = new Predmet(ID.split(" ")[0], name, parseInt(ID.split(" ")[1]));
 						pre.otvoren = true;
 						predmeti1i.push(pre);
 					}
@@ -46,9 +50,9 @@ window.onload = function(){
 				var predmeti2z = [];
 				var zad = jQuery("#vtora ul li");
 				for(var i = 0; i < zad.length; i++){
-					var ID = zad[i].firstChild.attributes[0].nodeValue;
+					var ID = zad[i].firstChild.getAttribute("title");
 					var name = zad[i].firstChild.innerHTML;
-					var pre = new Predmet(ID.split(" ")[0], name, ID.split(" ")[1]);
+					var pre = new Predmet(ID.split(" ")[0], name, parseInt(ID.split(" ")[1]));
 					pre.otvoren = false;
 					predmeti2z.push(pre);
 				}
@@ -56,9 +60,9 @@ window.onload = function(){
 				var predmeti2i = [];
 				for(var i = 0; i < izb.length; i++){
 					if(izb[i].checked){
-						var ID = izb[i].attributes[1].nodeValue;
+						var ID = izb[i].getAttribute("title");
 						var name = izb[i].nextSibling.nodeValue;
-						var pre = new Predmet(ID.split(" ")[0], name, ID.split(" ")[1]);
+						var pre = new Predmet(ID.split(" ")[0], name, parseInt(ID.split(" ")[1]));
 						pre.otvoren = false;
 						predmeti2i.push(pre);
 					}
@@ -66,9 +70,9 @@ window.onload = function(){
 				var predmeti3z = [];
 				var zad = jQuery("#treta ul li");
 				for(var i = 0; i < zad.length; i++){
-					var ID = zad[i].firstChild.attributes[0].nodeValue;
+					var ID = zad[i].firstChild.getAttribute("title");
 					var name = zad[i].firstChild.innerHTML;
-					var pre = new Predmet(ID.split(" ")[0], name, ID.split(" ")[1]);
+					var pre = new Predmet(ID.split(" ")[0], name, parseInt(ID.split(" ")[1]));
 					pre.otvoren = false;
 					predmeti3z.push(pre);
 				}
@@ -76,9 +80,9 @@ window.onload = function(){
 				var predmeti3i = [];
 				for(var i = 0; i < izb.length; i++){
 					if(izb[i].checked){
-						var ID = izb[i].attributes[1].nodeValue;
+						var ID = izb[i].getAttribute("title");
 						var name = izb[i].nextSibling.nodeValue;
-						var pre = new Predmet(ID.split(" ")[0], name, ID.split(" ")[1]);
+						var pre = new Predmet(ID.split(" ")[0], name, parseInt(ID.split(" ")[1]));
 						pre.otvoren = false;
 						predmeti3i.push(pre);
 					}
@@ -88,14 +92,50 @@ window.onload = function(){
 				var name = jQuery("#diplomskateks").val()
 				var diplomska = new Predmet(ID,name,8);
 				ply = new Player();
-				ply.init(predmeti1i, predmeti2i, predmeti3i, predmeti1z, predmeti2z, predmeti3z, diplomska, namePly, ball, base, 200,1000,"#EEEEEE");
+				ply.init(predmeti1i, predmeti2i, predmeti3i, predmeti1z, predmeti2z, predmeti3z, diplomska, namePly, ball, base, canvas.height,canvas.width,"#EEEEEE");
 				draw = true;
-				jQuery(this).dialog("close");									
+				jQuery(this).dialog("close");													
+				}else{
+					if(accordion == 0){
+						var sum = 0;
+						var pred = jQuery("#prva input[type=checkbox]");
+						for(var i = 0; i < pred.length; i++){
+							if(pred[i].checked){
+								sum++;
+							}
+						}
+						if(sum == 6){
+							jQuery("#accordion").accordion("activate",++accordion);
+						}
+					}else if(accordion == 1){
+						var sum = 0;
+						var pred = jQuery("#vtora input[type=checkbox]");
+						for(var i = 0; i < pred.length; i++){
+							if(pred[i].checked){
+								sum++;
+							}
+						}
+						if(sum == 6){
+							jQuery("#accordion").accordion("activate",++accordion);
+						}
+					}else{
+						var sum = 0;
+						var pred = jQuery("#treta input[type=checkbox]");
+						for(var i = 0; i < pred.length; i++){
+							if(pred[i].checked){
+								sum++;
+							}
+						}
+						if(sum == 6){
+							jQuery("#accordion").accordion("activate",++accordion);
+						}
+					}
+									
+				}
 			}
 		}],
 		open: function(event, ui) {$(this).parent().children().children('.ui-dialog-titlebar-close').hide();},
 		close: function() {
-                allFields.val( "" ).removeClass( "ui-state-error" );
             },
 	});
 	jQuery("#dialog").dialog("open");
@@ -107,10 +147,14 @@ window.onload = function(){
 	window.paper.view.onFrame = function(event){
 		if(draw){
 			ply.ball.moveBall();
-			ply.ball.checkTouchBase(base);
-			ply.ball.touchWalls(500,1000);
+			ply.ball.checkTouchBase(ply.base);
+			ply.ball.touchWalls(canvas.height,canvas.width);
 			ply.map.touchElement(ply.ball,ply.base);
 			ply.base.move(basex);
+			ply.map.moveDiplomska();
+			if(ply.gameOver()){
+				draw = false;
+			}
 		}		
 	}
 	/*function frame(event){
@@ -122,6 +166,14 @@ window.onload = function(){
 	}*/
 	
 	tool.onMouseMove = function(event){
-		basex = event.point.x;
+		if(draw){
+		if(event.point.x - ply.base.base.width/2 <= 0){
+			basex = ply.base.base.width/2;
+		}else if(event.point.x + ply.base.base.width/2 >= canvas.width){
+			basex = canvas.width - ply.base.base.width/2;
+		}else{
+			basex = event.point.x;
+		}
+		}	
 	}
 }
